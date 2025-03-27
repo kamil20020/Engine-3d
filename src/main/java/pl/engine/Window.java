35c7d2e86924090ab74d9c726b16b2a9e40e20d9
kdywan_254049 <idywan_254049@e-science.pl>
2025-03-27 23:16:46 +0100

    Added key listeners

--- src/main/java/pl/engine/Window.java
+++ src/main/java/pl/engine/Window.java
@@ -2,9 +2,11 @@ package pl.engine;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
-public class Window extends JFrame {
+public class Window extends JFrame implements KeyListener {
 
     public Window() {
         super();
@@ -13,10 +15,47 @@ public class Window extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setExtendedState(MAXIMIZED_BOTH);
+        addKeyListener(this);
 
         Screen screen = Screen.getInstance();
         add(screen);
 
         screen.initDraw();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int pressedKeyCode = e.getKeyCode();
+
+        switch (pressedKeyCode){
+
+            case KeyEvent.VK_LEFT:
+                System.out.println("Left");
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                System.out.println("Right");
+                break;
+
+            case KeyEvent.VK_UP:
+                System.out.println("Top");
+                break;
+
+            case KeyEvent.VK_DOWN:
+                System.out.println("Down");
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
